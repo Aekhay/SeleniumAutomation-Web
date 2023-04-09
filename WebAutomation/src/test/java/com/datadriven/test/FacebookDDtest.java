ca8083106f8c457a5b7a3d72625bf02e97541720
wrp <wrp@localhost>
2023-04-09 15:26:52 +0500

    Implemented Excel util

---
+++ WebAutomation/src/test/java/com/datadriven/test/FacebookDDtest.java
@@ -0,0 +1,70 @@
+package com.datadriven.test;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.test.utility.TestUtil;
+
+public class FacebookDDtest {
+	
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setUp() {
+		
+		
+		//For MAC chrome driver permission terminal command :  xattr -d com.apple.quarantine chromedriver
+		System.setProperty("webdriver.chrome.driver", "/Users/wrp/git/SeleniumAutomation-Web/WebAutomation/src/test/resources/Drivers/chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		
+		driver.get("https://www.facebook.com/");
+	}
+	
+	
+	@DataProvider
+	public Iterator<Object[]> getTestData() {
+		
+		ArrayList<Object[]> testData = TestUtil.getDataFromExcel();
+		return testData.iterator();
+	}
+	
+	
+	@Test (dataProvider = "getTestData")
+	public void FacebookloginTest(String username, String password) throws InterruptedException {
+		
+		WebElement usr  = driver.findElement(By.xpath("//input[@id='email']"));
+		usr.sendKeys(username);
+		
+		WebElement pass  = driver.findElement(By.xpath("//input[@id='pass']"));
+		pass.sendKeys(password);
+		
+		Thread.sleep(2000);
+		
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		
+		if (driver != null) {
+			
+			driver.quit();
+		}
+	}
+
+}
---
+++ WebAutomation/src/test/java/com/test/utility/TestUtil.java
@@ -0,0 +1,43 @@
+package com.test.utility;
+
+import java.util.ArrayList;
+
+import com.test.excelutil.Xls_Reader;
+
+public class TestUtil {
+
+	
+	public static Xls_Reader reader;
+	
+	public static ArrayList<Object[]> getDataFromExcel(){
+		ArrayList<Object[]> myData = new ArrayList<Object[]>();
+		
+		try {
+			
+			reader = new Xls_Reader("/Users/wrp/git/SeleniumAutomation-Web/WebAutomation/src/test/resources/Testdata/FacebookTestdata.xlsx");
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		String sheetName = "RegTestData";
+		
+		int rowCount = reader.getRowCount(sheetName);
+
+		for(int rowNum=2; rowNum<=rowCount; rowNum++) {
+			
+			String username = reader.getCellData("RegTestData", "username", rowNum);
+			
+			String password  = reader.getCellData("RegTestData", "password", rowNum);
+			
+			Object ob[] = {username , password};
+			
+			myData.add(ob);
+			
+		}
+		return myData;
+		
+		
+	}
+}
